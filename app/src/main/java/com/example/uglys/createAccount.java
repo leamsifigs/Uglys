df7e703620407ee548677f2033c988cbe2f63704
jchav97 <jchavez97@outlook.com>
2019-04-27 10:21:37 -0700

    added unique radio button feature to create an account page

--- app/src/main/java/com/example/uglys/createAccount.java
+++ app/src/main/java/com/example/uglys/createAccount.java
@@ -11,8 +11,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class createAccount extends AppCompatActivity {
+
+    private RadioGroup radioUserGroup;
+    private RadioButton radioConsumerButton;
+    private RadioButton radioProducerButton;
+    private EditText mEditTextFirstName;
+    private EditText mEditTextLastName;
+    private EditText mEditTextEmail;
+    private EditText mEditTextPhoneNumber;
+    private EditText mEditTextStreetAddress;
+    private EditText mEditTextCity;
+    private EditText mEditTextState;
+    private EditText mEditTextCountry;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +39,26 @@ public class createAccount extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         Button buttonSubmit = (Button)findViewById(R.id.Submit);
 
         buttonSubmit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+
+                radioUserGroup = (RadioGroup) findViewById(R.id.radioUser);
+                radioConsumerButton = (RadioButton) findViewById(R.id.Consumer_Button);
+                radioProducerButton = (RadioButton) findViewById(R.id.Producer_Button);
+                mEditTextFirstName = (EditText)findViewById(R.id.User_First_Name);
+                mEditTextLastName = (EditText)findViewById(R.id.User_Last_Name);
+                mEditTextEmail = (EditText)findViewById(R.id.User_Email);
+                mEditTextPhoneNumber = (EditText)findViewById(R.id.User_Phone_Number);
+                mEditTextStreetAddress = (EditText)findViewById(R.id.User_Street_Address);
+                mEditTextCity = (EditText)findViewById(R.id.User_City);
+                mEditTextState = (EditText)findViewById(R.id.User_State);
+                mEditTextCountry = (EditText)findViewById(R.id.User_Country);
+
+
                 Intent intent = new Intent(createAccount.this, MainActivity.class);
                 startActivity(intent);
             }
